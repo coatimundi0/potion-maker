7a44f16761791f1616e64474ab272ccfdfd81e9e
garo martinez <62900447+coatimundi0@users.noreply.github.com>
2025-02-12 09:44:50 -0500

    Update Accuracy.py
    how do you delete files :/

--- Accuracy.py
+++ Accuracy.py
@@ -1,31 +1,4 @@
-from random import randrange 
-from random import randint
-from random import choice
-
 '''
-ACCURACY (If Given Title)
-    YES --> Accurate
-    NO  --> Opposite
+GITHUB ISNT LETTING ME DELETE!!!
+    this has been merged into the Others.py file :)
 '''
-accurate = ["Seems legit.", "This feels extra magical.", "This feels weird...", "You suppose you can trust it."]
-
-##### ##### ##### ##### ##### ##### ##### ##### ##### ##### 
-
-def pickAccuracy():
-    # accuracy ::  ACCURATE (0-89); OPPOSITE (90-100)
-    accuracy = randint(0, 99)
-
-    if 0 <= accuracy <= 89:
-        phrase = accurate[randrange(0, len(accurate)-1)]
-        deter = bool(True)
-    elif 90 <= accuracy <= 99:
-        acc = randint(0, 99)
-        phrase = f"There is a {acc}% chance that the item will have the opposite effect. Roll 1d100 and get higher than {acc}."
-        deter =  bool(False)
-    else: 
-        print("pickAccuracy ERROR")
-        return -1
-
-    return phrase
-
-##### ##### ##### ##### ##### ##### ##### ##### ##### #####
