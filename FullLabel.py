46edd857ecce04260805919a405fd5fb466172ee
garo martinez <62900447+coatimundi0@users.noreply.github.com>
2025-02-12 09:46:24 -0500

    Update FullLabel.py

--- FullLabel.py
+++ FullLabel.py
@@ -1,31 +1,4 @@
-from random import randrange 
-from random import randint
-
 '''
-FULL TITLE
-    The label reads [TITLE] of [EFFECT].
-    The label reads [EFFECT] [TITLE].
+GITHUB ISNT LETTING ME DELETE!!!
+    this is now the Titles.py file :)
 '''
-
-phrasing = ["The label says it's called ", "The label reads ",
-            "It says ", "It's called ", "Apparently it's called ", 
-            "The name of it is ", "The name is ", "The name says "]
-
-##### ##### ##### ##### ##### ##### ##### ##### ##### ##### 
-
-def makeFullTitle(title, effect):    
-    verbiage = randint(0, 1)
-    says = phrasing[randrange(0, len(phrasing)-1)]
-    phrase = ""
-
-    if verbiage == 0:
-        phrase = says + title + " of " + effect[0] + "."
-    elif verbiage == 1: 
-        phrase = says + effect[0] + " " + title + "."
-    else: 
-        print("makeFullTitle ERROR")
-        return -1
-    
-    return phrase
-
-##### ##### ##### ##### ##### ##### ##### ##### ##### ##### 
