8b5b09dff988dbe6bfee8dff0d69b1de00f8e7fe
garo martinez <62900447+coatimundi0@users.noreply.github.com>
2025-02-12 09:27:47 -0500

    Update Form.py

--- Form.py
+++ Form.py
@@ -7,50 +7,81 @@ FORM
     Looking closely, you're able to figure out that it is a powder that [POWDER].
     It's a [PASTE], so you know that the only useful way to use it is to smear it onto a target.
     It's a [GEL], so you know that the only useful way to use it is to rub it onto a target.
+
+    0        |   1        |   2       |   3         
+    LIQUID   |   POWDER   |   PASTE   |   GEL        
 '''
-    # LIQUID
-ingestible = "is ingested by the user."
-splash = "is used as a ranged attack at the target."
-droplet = "is that is dripped onto a target."
-liquid = [ingestible, splash, droplet]
-    # POWDER
-puff_powder = "is blown onto a target or area of effect."
-sprinkle_powder = "is sprinkled onto a target or area of effect."
-edible_powder = "is ingested by the user."
-ingredient_powder = "is mixed into something to cause an effect."
 
 ##### ##### ##### ##### ##### ##### ##### ##### ##### ##### 
 
-def makeForm():
-    # form :: LIQUID (0); POWDER (1); PASTE (2); GEL (3)
+def make_form():
+        # random form
     form_type = randint(0, 3)
+        # is it edible?
     edible = bool(False)
 
+
+        # LIQUID (0) ::: 
     if form_type == 0:
+            # phrases to be used
+        ingestible = "is ingested by the user."
+        splash = "is used as a ranged attack at the target."
+        droplet = "is that is dripped onto a target."
         form = [ingestible, splash, droplet]
+        
+            # determine the full phrase at random
         liquid = form[randrange(0, len(form)-1)]
         phrase = "Based on the container, this " + liquid
+
+            # ingestible = true // otherwise = false
         if liquid == form[0]:
             edible = bool(True)
         else:
             pass
+
+
+        # POWDER (1) :::
     elif form_type == 1:
-        form = [edible_powder, puff_powder, sprinkle_powder, ingredient_powder]
+            # phrases to be used
+        puffPowder = "is blown onto a target or area of effect."
+        sprinkePowder = "is sprinkled onto a target or area of effect."
+        ediblePowder = "is ingested by the user."
+        ingredientPowder = "is mixed into something to cause an effect."
+        form = [ediblePowder, puffPowder, sprinkePowder, ingredientPowder]
+
+            # determine the full phrase at random
         powder = form[randrange(0, len(form)-1)]
-        phrase = "Looking closely, you can tell it " + powder
+        phrase = "Just by looking, you can tell it " + powder
+
+            # ingestible = true // otherwise, false
         if powder == form[0]:
             edible = bool(True)
         else:
             pass
+
+
+        # PASTE (2) :::
     elif form_type == 2:
         phrase = "The only real way to use this is to smear it onto a target."
+
+
+        # GEL (3) :::
     elif form_type == 3:
         phrase = "The best way to use something like this is to rub it onto a target."
+
+
+        # ERROR :::
     else:
-        print("makeForm ERROR")
+        print("make_form ERROR")
         return -1
 
+
+        # returns: integer (0-3) / string / bool
     final = [form_type, phrase, edible]
     return final
     
 ##### ##### ##### ##### ##### ##### ##### ##### ##### ##### 
+
+    # DELETE LATER: for testing purposes
+test = make_form()
+#print(test[1])
