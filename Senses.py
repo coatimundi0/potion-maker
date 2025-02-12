06bafa2ebe173656e2cf9da83ed6c5e2905a44a0
garo martinez <62900447+coatimundi0@users.noreply.github.com>
2025-02-12 09:28:06 -0500

    Update Senses.py

--- Senses.py
+++ Senses.py
@@ -1,6 +1,8 @@
-from random import randrange 
+from random import randrange
 from random import randint
 from random import choice
+import pandas as pd
+import os
 
 '''
 ADVERB + SMELLS & TASTES
@@ -9,62 +11,108 @@ ADVERB + SMELLS & TASTES
 
     It smells [adverb] [specific].
     It tastes [adverb] [specific].
+
+    0         |   1       |   2     
+    ADVERBS   |   SENSE   |   NOADVERB
 '''
-
-adverb = ["a lot like", "a little like", "strongly of", "pungent like", "like", "intoxicatingly like", "like spice mixed with", "like fruit mixed with", "like it's mixed with", "crisply like", "faintly like", "subtly like"]
-
-no_adverb = ["nothing at all.", "strong... but pleasant?", "somewhat comforting... reminds you of a time where you felt safe.", "delicate. It's subtle.", "faint.", "heady.", "pungent.", "rich.", "intoxicating... how lovely...", "spicy. It's got a punch to it!", "piquant. It stings your nostrils.", "heavy.", "laden.", "fetid... like death.", "acrid and bitter.", "funky! In a bad way!", "funky! In a good way!", "unbelievably nasty.", "RANK! Yuck!", "ripe.", "sickly...", "super sour.", "kind of stale.", "super delicious. You're satisfied.", "lovely and fresh.", "crisp and sharp.", "savoury.", "tangy. Ooh!", "citrusy... you can't put your finger on it...", "coppery.", "earthy. Like soil.", "medicinal.", "smoky.", "woody."]
-
-specific = ["an antiseptic. Very medical.", "raw fish.", "an apple.", "garlic.", "flowers.", "a hyacinth.", "jasmine flowers.", "freshly brewed tea.", "green tea.", "honeysuckle.", "nightshade..? Oh dear. Hopefully that's not bad.", "roses.", "cooked fish. Weird.", "cooked chicken.", "cooked beef.", "sweet basil.", "yummy chives.", "cilantro. (Does it taste like soap or onions to you?)", "salsa. Ooh!", "dillweed.", "fern.", "knotted marjoram.", "peppermint.", "spearmint.", "oregano!", "flat-leaf parsley.", "rosemary. Witchy.", "sage. You feel cleansed...", "tarragon.", "thyme.", "blood... oh wow.", "metal.", "steel. Maybe that's just nearby armor you're smelling...", "iron. Fae beware!", "gold. Who knew gold had a smell?", "Gaian za'atar.", "bananas!", "coconuts... tropical!", "almonds. Hope you're not allergic."]
 
 ##### ##### ##### ##### ##### ##### ##### ##### ##### ##### 
 
-def hasAdverb():
-    descriptive = choice([True, False])
-    return descriptive
+basePath = os.path.dirname(os.path.realpath(__file__))
+fileName = "AllSenses.csv"
+fullPath = os.path.join(basePath, fileName)
+df = pd.read_csv(fullPath, sep=';')
 
-def makeSmell():
-    describe = hasAdverb()
-    phrase = ""
+def pick_sense(describe):
+    sense = randint(0, 1)
+    verb = ""
 
+        # ADVERB (true) :::
     if describe == bool(True):
-        verbiage = adverb[randrange(0, len(adverb)-1)]
-        smell = specific[randrange(0, len(specific)-1)]
-        phrase = "It smells " + verbiage + " " + smell
+        match sense:
+                # TASTE (0) :::
+            case 0:
+                verb = "tastes"
+                # SMELL (1) :::
+            case 1:
+                verb = "smells"
+                # ERROR :::
+            case _:
+                print("pick_sense ERROR1")
+                return -1
+
+        #  NOADVERB (false)
     elif describe == bool(False):
-        smell = no_adverb[randrange(0, len(no_adverb)-1)]
-        phrase = "The smell is " + smell
+        match sense:
+                # TASTE (0) :::
+            case 0:
+                verb = "taste"
+                # SMELL (1) :::
+            case 1:
+                verb = "smell"
+                # ERROR :::
+            case _:
+                print("pick_sense ERROR2")
+                return -1
+
+        # ERROR :::
     else:
-        print("makeSmell ERROR")
+        print("pick_sense ERROR3")
         return -1
     
-    final = [phrase, smell]
+    return verb
+    
+
+def make_sense():
+        # DESCRIPTORS
+    describe = choice([True, False])
+    phrase = ""
+    desc = ""
+
+        # ADVERB (true) :::
+    if describe == bool(True):
+                # ADVERB (0) :::
+            # determine row length
+        rowLen = df[df.columns[0]].count()
+                # determine the random point
+        rowNum = randrange(0, (rowLen-1))
+                # determine adverb
+        adverb = df.iloc[rowNum, 0]
+
+                # SCENT (1) :::
+            # determine row length
+        rowLen = df[df.columns[1]].count()
+            # determine the random point
+        rowNum = randrange(0, (rowLen-1))
+           # determine sense
+        desc = df.iloc[rowNum, 1]
+            # determine verb
+        verb = pick_sense(describe)
+
+        phrase = "It " + verb + " " + adverb + " " + desc
+
+        # NOADVERB (false) :::
+    elif describe == bool(False):
+            # determine row length
+        rowLen = df[df.columns[2]].count()
+                # determine the random point
+        rowNum = randrange(0, (rowLen-1))
+                # determine scent
+        desc = df.iloc[rowNum, 2]
+            # determine verb
+        verb = pick_sense(describe)
+        phrase = "The " + verb + " is " + desc
+
+        # ERROR :::
+    else:
+        print("make_sense ERROR2")
+        return -1
+    
+    final = [phrase, desc]
     return final
 
-def makeTaste(smell):
-     # adverb?
-    describe = hasAdverb()
-    if describe == bool(True):
-        verbiage = adverb[randrange(0, len(adverb)-1)]
-        taste = specific[randrange(0, len(specific)-1)]
-        phrase = "It tastes " + verbiage + " " + taste
-    elif describe == bool(False):
-        taste = no_adverb[randrange(0, len(no_adverb)-1)]
-        phrase = "The taste is " + taste
-    else:
-        print("makeTaste ERROR 2")
-        return -1
+##### ##### ##### ##### ##### ##### ##### ##### ##### ##### 
 
-    # unique smell?
-    det = smell[1]
-    if taste == det:
-        phrase = "It tastes exactly like it smells, for better or worse."
-    elif taste != det:
-        pass
-    else: 
-        print("makeTaste ERROR 1")
-        return -1
-
-    return phrase
-
-##### ##### ##### ##### ##### ##### ##### ##### ##### #####
+    # DELETE LATER: for testing purposes
+test = make_sense()
+print(test[0])
